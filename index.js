96c43501ee135d9ead6be88f7c2ab794fe55da4f
Nelson Parra <nlparra.dev@gmail.com>
2020-03-07 12:11:08 -0500

    javaScript code almost done and css almost done

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const getRamdom = r => {
   return Math.floor(Math.random() * r);
-};
-
-let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-
-window.onload = function() {
-    document.getElementById("icon").innerHTML=icons[getRamdom(icons.length)];
-    document.getElementById("number").innerHTML=num[getRamdom(num.length)];
-  
+};let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];window.onload = function() {
+	let iramdom, n;
+	iramdom=getRamdom(icons.length-1);
+	console.log(iramdom);
+    let classicons = document.getElementsByClassName("icon");
+	//console.log(classicons);
+	for(i in classicons){			
+	classicons[i].innerHTML=icons[iramdom];
+		//console.log(classicons[i]);
+		if(iramdom===2||iramdom===3){
+			classicons[i].style.color = "red";
+			console.log(classicons[i]);
+		}
+	}		
+    document.getElementById("number").innerHTML=num[getRamdom(num.length-1)];
 };
 
 
