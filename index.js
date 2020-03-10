fecd446c0e6b9b75cc415b9ebce37c1967585d20
Nelson Parra <nlparra.dev@gmail.com>
2020-03-10 19:56:26 -0400

    fix styling on page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const getRamdom = r => {
   return Math.floor(Math.random() * r);
-};let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];window.onload = function() {
-	let iramdom, n;
+};
+
+
+let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+window.onload = function() {
+	let iramdom,n;
 	iramdom=getRamdom(icons.length-1);
 	console.log(iramdom);
     let classicons = document.getElementsByClassName("icon");
