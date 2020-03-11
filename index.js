3d26ccf639409584429eb2a874f48da4061d1f3c
Nelson Parra <nlparra.dev@gmail.com>
2020-03-10 20:16:25 -0400

    fix js

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const getRamdom = r => {
-  return Math.floor(Math.random() * r);
-};
-
-
-let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-
-window.onload = function() {
-	let iramdom,n;
-	iramdom=getRamdom(icons.length-1);
-	console.log(iramdom);
-    let classicons = document.getElementsByClassName("icon");
-	//console.log(classicons);
-	for(i in classicons){			
-	classicons[i].innerHTML=icons[iramdom];
-		//console.log(classicons[i]);
-		if(iramdom===2||iramdom===3){
-			classicons[i].style.color = "red";
-			console.log(classicons[i]);
-		}
-	}		
-    document.getElementById("number").innerHTML=num[getRamdom(num.length-1)];
-};
-
+	return Math.floor(Math.random() * r);
+  };
+  
+  let icons = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  
+  let iconsRamdom = () =>{
+	  
+	  let classicons = document.getElementsByClassName("icon");	
+	  let iramdom=getRamdom(icons.length);
+	  
+	  for(i in classicons){			
+	  classicons[i].innerHTML=icons[iramdom];
+		  //console.log(classicons[i]);
+		  if(iramdom===2||iramdom===3){
+			  classicons[i].style.color = " red";
+			  console.log(classicons[i]);
+		  }
+	  }
+	  return;	
+  }
+  
+  let numRamdoms = () =>{
+	  
+	  let n= getRamdom(num.length);	
+	  document.getElementById("number").innerHTML=num[n];
+	  
+  }
+  
+  window.onload = () => {
+	  numRamdoms();
+	  iconsRamdom();	
+  }
 
 
 
